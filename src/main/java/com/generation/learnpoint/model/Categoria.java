501d04b5e9c3964434855cf94a1c566bc2e76254
Alessandro Castro <97323030+alecfonseca@users.noreply.github.com>
2022-02-25 13:58:26 -0300

    task4

--- src/main/java/com/generation/learnpoint/model/Categoria.java
+++ src/main/java/com/generation/learnpoint/model/Categoria.java
@@ -14,44 +14,44 @@ import javax.validation.constraints.Size;
 @Table(name = "tb_categoria")
 public class Categoria {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo é obrigatório!")
+	@Size(min = 3, max = 50, message = "O nome da categoria deve ter entre 3 e 50 caracteres")
+	private String nomec;
+	
+	@NotBlank(message = "O atributo é obrigatório!")
+	@Size(min = 30, max = 1000, message = "O nome da categoria deve ter entre 30 e 1000 caracteres")
+	private String descricaoc;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	public Long getId() {
+		return id;
+	}
 
-    @NotBlank(message = "O atributo é obrigatório!")
-    @Size(min = 3, max = 50, message = "O nome da categoria deve ter entre 3 e 50 caracteres")
-    private String nome_c;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    @NotBlank(message = "O atributo é obrigatório!")
-    @Size(min = 30, max = 1000, message = "O nome da categoria deve ter entre 30 e 1000 caracteres")
-    private String descricao_c;
+	public String getNomec() {
+		return nomec;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setNomec(String nomec) {
+		this.nomec = nomec;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getDescricaoc() {
+		return descricaoc;
+	}
 
-    public String getNome_c() {
-        return nome_c;
-    }
-
-    public void setNome_c(String nome_c) {
-        this.nome_c = nome_c;
-    }
-
-    public String getDescricao_c() {
-        return descricao_c;
-    }
-
-    public void setDescricao_c(String descricao_c) {
-        this.descricao_c = descricao_c;
-    }
-
-
-
-
+	public void setDescricaoc(String descricaoc) {
+		this.descricaoc = descricaoc;
+	}
+	
+	
+	
+	
 }
