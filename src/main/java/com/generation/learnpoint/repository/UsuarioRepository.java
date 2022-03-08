cc58f745cdaccd775fdef411d90aeda948ed13dc
mayaratlt <mayaratlt23@gmail.com>
2022-03-08 12:04:00 -0300

    Task6 Mayara

---
+++ src/main/java/com/generation/learnpoint/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.generation.learnpoint.repository;
+
+import java.util.Optional;
+
+import com.generation.learnpoint.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	
+	public Optional<Usuario> findByLogin(String login);
+
+}
