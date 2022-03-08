39d651c17f83fe50df2973d4b79b1aa99400036e
Natalia <natalia.s36.ns@gmail.com>
2022-03-08 15:00:09 -0300

    CommitNatalia

--- src/main/java/com/generation/learnpoint/model/Usuario.java
+++ src/main/java/com/generation/learnpoint/model/Usuario.java
@@ -29,7 +29,7 @@ public class Usuario {
 	
 	@NotBlank(message = "O atributo é obrigatório!")
 	@Email( message = "O login deve ser um E-mail.")
-	private String login;
+	private String usuario;
 	
 	@NotBlank(message = "O atributo é obrigatório!")
 	@Size(min = 8, message = "A senha deve conter minimo de 8 caracteres")
@@ -57,12 +57,12 @@ public class Usuario {
 		this.nomeu = nomeu;
 	}
 
-	public String getLogin() {
-		return login;
+	public String getUsuario() {
+		return usuario;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
+	public void setUsuarion(String usuario) {
+		this.usuario = usuario;
 	}
 
 	public String getSenha() {
@@ -71,5 +71,10 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public void setToken(String gerarBasicToken) {
+		// TODO Auto-generated method stub
+		
 	}
 }
